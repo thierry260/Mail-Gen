60fa0017cd79e2e6a4c10da2a310a639769ca3fd
mvereijken <m.vereijken@hotmail.nl>
2024-11-06 14:02:36 +0100

    Subscription changes

--- src/routes/api/create-checkout-session/+server.js
+++ src/routes/api/create-checkout-session/+server.js
@@ -8,7 +8,8 @@ const stripe = new Stripe(process.env.VITE_STRIPE_SECRET_KEY);
 export async function POST({ request }) {
   console.log("ENV KEY: ", process.env.VITE_STRIPE_SECRET_KEY);
   try {
-    const { priceId, email, workspaceId, userId } = await request.json();
+    const { priceId, email, workspaceId, userId, trialDaysLeft } =
+      await request.json();
 
     const customer = await stripe.customers.create({ email });
     const customerId = customer.id;
@@ -17,20 +18,33 @@ export async function POST({ request }) {
       [`users.${userId}`]: { stripeCustomerId: customerId },
     });
 
-    // Calculate the trial end date (14 days from now)
-    const trialEnd = Math.floor(Date.now() / 1000) + 14 * 24 * 60 * 60; // 14 days in seconds
-
-    const session = await stripe.checkout.sessions.create({
+    const sessionData = {
       customer: customerId,
       payment_method_types: ["card", "ideal"],
       mode: "subscription",
       line_items: [{ price: priceId, quantity: 1 }],
       success_url: `http://app.mailgen.nl/settings?tab=subscription&payment=success&cid=${customerId}`,
       cancel_url: `http://app.mailgen.nl/settings?tab=subscription&payment=cancel`,
-      subscription_data: {
-        trial_end: Math.floor(Date.now() / 1000) + 14 * 24 * 60 * 60 + 60, // 14 days in seconds
-      },
-    });
+      subscription_data: {},
+    };
+
+    // Calculate the remaining seconds of today
+    const now = new Date();
+    const endOfDay = new Date(now);
+    endOfDay.setHours(23, 59, 59, 999); // Set to the last millisecond of the current day
+    const remainingSecondsToday = Math.floor((endOfDay - now) / 1000); // Time left in seconds
+
+    // Only add trial_end if trialDaysLeft is greater than zero
+    if (trialDaysLeft > 0) {
+      const trialEndTime =
+        Math.floor(Date.now() / 1000) +
+        remainingSecondsToday +
+        trialDaysLeft * 24 * 60 * 60 +
+        60;
+      sessionData.subscription_data.trial_end = trialEndTime;
+    }
+
+    const session = await stripe.checkout.sessions.create(sessionData);
 
     if (!session || !session.id) {
       throw new Error(
